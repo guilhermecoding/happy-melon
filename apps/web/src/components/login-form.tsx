a4017d759c95ebf334cf6ab35be07218718e9d32
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-07-17 23:08:52 -0300

    :sparkles: feat: Adição de novos componentes de UI e página de login, incluindo logo e imagem de fundo

---
+++ apps/web/src/components/login-form.tsx
@@ -0,0 +1,79 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import Image from "next/image"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <Image
+                  src="/logo-texto.svg"
+                  alt="Logo Happy Melon"
+                  className="w-auto h-26 object-contain"
+                  width={100}
+                  height={100}
+                  loading="eager"
+                />
+                <p className="text-balance text-base text-muted-foreground">
+                  Entre com as credenciais da maratona
+                </p>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+              </Field>
+              <FieldDescription className="text-center">
+                Don&apos;t have an account? <a href="#">Sign up</a>
+              </FieldDescription>
+            </FieldGroup>
+          </form>
+          <div className="relative hidden bg-muted md:block">
+            <Image
+              src="/thumb_todos.jpg"
+              alt="Imagem de login"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              width={1000}
+              height={1000}
+              loading="eager"
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
