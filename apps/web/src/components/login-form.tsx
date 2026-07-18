0ff7007fb50bc37ccbd17b1e26bdb2779fcc373d
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-07-18 00:29:36 -0300

    :lipstick: feat: Atualização do componente LoginForm para adicionar bordas arredondadas ao cartão

--- apps/web/src/components/login-form.tsx
+++ apps/web/src/components/login-form.tsx
@@ -19,7 +19,7 @@ export function LoginForm({
 }: React.ComponentProps<"div">) {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card className="overflow-hidden p-0 shadow-none border-b-8 border-l-8">
+      <Card className="overflow-hidden p-0 shadow-none border-b-8 border-l-8 rounded-2xl">
         <CardContent className="grid p-0 md:grid-cols-2">
           <form className="p-6 md:p-8">
             <FieldGroup>
