62f2890d4fd37388edbefa2f3db4431bf843b4ca
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-07-24 21:36:44 -0300

    :sparkles: feat: Adição de variáveis de cor para bordas e atualização do componente BoxOption para incluir navegação, melhorando a estética e a usabilidade da interface administrativa.

--- apps/web/src/app/admin/home/_components/box-option.tsx
+++ apps/web/src/app/admin/home/_components/box-option.tsx
@@ -1,5 +1,6 @@
 import { ChampionIcon, UserMultipleIcon } from '@hugeicons/core-free-icons';
 import { HugeiconsIcon } from '@hugeicons/react';
+import Link from 'next/link';
 
 interface BoxOptionProps {
     title: string;
@@ -8,14 +9,18 @@ interface BoxOptionProps {
 
 export default function BoxOption({ title, type }: BoxOptionProps) {
     const iconElement = type === 'contests' ? ChampionIcon : UserMultipleIcon;
+    const href = type === 'contests' ? 'competicoes' : 'usuarios';
 
     return (
-        <div className='w-52 h-36 bg-muted p-4 rounded-4xl relative overflow-hidden'>
+        <Link
+            className='w-52 h-36 bg-muted p-4 rounded-4xl relative overflow-hidden border-4 border-border hover:border-border-hover transition-colors duration-300 cursor-pointer'
+            href={`/admin/${href}`}
+        >
             <h2 className='text-xl font-semibold'>{title}</h2>
             <HugeiconsIcon
                 icon={iconElement}
                 className='size-32 absolute -bottom-8 opacity-5 -right-5'
             />
-        </div>
+        </Link>
     )
 }
