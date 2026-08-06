1881cb05b6acdca2eb2018d7f90738fa043becde
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-08-05 22:48:34 -0300

    :lipstick: style: Atualização do componente CountdownContest para melhorar a apresentação visual, incluindo um título centralizado e ajustes nas classes de estilo do FlipClock, promovendo uma melhor responsividade e clareza na exibição da contagem regressiva.

--- apps/web/src/app/staff/_components/countdown-contest.tsx
+++ apps/web/src/app/staff/_components/countdown-contest.tsx
@@ -2,12 +2,17 @@ import FlipClock from '@/components/8starlabs-ui/flip-clock'
 
 export default function CountdownContest() {
     return (
-        <div className="relative flex h-screen w-full items-center justify-center">
-            <FlipClock
-                variant="default"
-                className="relative -top-1/5 text-5xl "
-                countdown
-            />
+        <div className="flex h-screen w-full items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-4 md:gap-6 relative -top-1/5">
+                <h1 className="text-xl md:text-3xl lg:text-4xl font-bold text-center text-muted-foreground">
+                    Começa em
+                </h1>
+                <FlipClock
+                    variant="muted"
+                    className="relative text-3xl md:text-5xl lg:text-7xl"
+                    countdown
+                />
+            </div>
         </div>
     )
 }
